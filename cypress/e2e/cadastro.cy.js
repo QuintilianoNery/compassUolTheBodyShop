bbd9c154508fa86d92f96164a9defd9ae10031e1
Quintiliano <quintiliano1995@gmail.com>
2023-09-15 16:30:30 -0300

    Realização do commit Inicial para estruturação do projeto

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+    //cy.intercept para aguardar o carregamento da página principal
+    //Não uszar page Objects
+    // No be visible ussar a asserção para clicar ou preencher os campos
+    //estudar boas práticas de desenvolvimento de testes com cypress
+  })
+})
